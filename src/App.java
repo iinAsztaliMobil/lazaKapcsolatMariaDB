da594e4ff90338e44adbfdeeb0ec9978fe130960
13asTerem <you@example.com>
2026-09-07 11:47:49 +0200

    statement

--- src/App.java
+++ src/App.java
@@ -2,7 +2,9 @@ import java.sql.Connection;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        MariaDB mariadb = new MariaDB();
-        Connection con = mariadb.connect();
+        DataService dataservice = new DataService(new MariaDB());
+        
+        // MariaDB mariadb = new MariaDB();
+        // Connection con = mariadb.connect();
     }
 }
--- src/DataService.java
+++ src/DataService.java
@@ -1,8 +1,33 @@
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class DataService {
 
     Database db;
 
     public DataService(Database db){
         this.db = db;
+    }
+
+     public ArrayList<Employee> getEmployees(){
+        try {
+            return tryGetEmployees();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return null;
+        }
+     }
+
+    public ArrayList<Employee> tryGetEmployees() throws Exception{
+        ArrayList<Employee> empList = new ArrayList<>();
+
+        //feltoltes
+        Connection con = db.connect();
+        String sql = "select * from employees";
+
+        Statement stmt = con.createStatement();
+
+        return empList;
     }
 }
