313fffcb94d7240913051429cfdef77354f7b10b
13asTerem <you@example.com>
2026-09-07 12:02:56 +0200

    laza fuggoseg pt2

--- src/App.java
+++ src/App.java
@@ -3,7 +3,8 @@ import java.sql.Connection;
 @SuppressWarnings("unused")
 public class App {
     public static void main(String[] args) throws Exception {
-        DataService dataservice = new DataService(new MariaDB());
+        // DataService dataservice = new DataService(new MariaDB());
+        DataService dataservice = new DataService(new SQLite());
         
         // MariaDB mariadb = new MariaDB();
         // Connection con = mariadb.connect();
